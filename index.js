1558e0031b93933b41664b6b19296baffedb60be
elifintizamoglu <elifintizam@gmail.com>
2023-09-27 21:47:37 +0300

    Hello Node.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ console.log("Hello Node");
 
 var slugify = require('slugify')
 
-const title = slugify('some string') // some-string
+const title = slugify('Hello Node.js') // some-string
 
 console.log(title)
