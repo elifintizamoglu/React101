265fe2ebc1516544cbcfe862eb65becb4fb48329
elifintizamoglu <elifintizam@gmail.com>
2023-10-03 20:38:27 +0300

    Counter Exercise

---
+++ react-project/src/components/Counter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const decrease = () => {
+        setCount(count - 1);
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+        </div>
+
+
+    );
+}
+
+export default Counter;
